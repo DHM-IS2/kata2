f1be6b0777459a774ecdda6870d4b048304535fd
Darwin <Darwin@Darwin-EHM-PC>
2013-12-02 20:38:51 +0000

    Primer intento (List)

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -2,7 +2,7 @@ package kata2;
 
 public class Kata2 {
     public static void main(String[] args){
-       int[] vec = {0,0,0,0,4,4,4,4,4};
+       int[] vec = {0,1,1,2,3};
        System.out.println("El número más repetido es el " + MostRepeated.mostRepeated(vec));
     }
 }
